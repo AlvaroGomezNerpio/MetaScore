7667d0a1889bcb8198912577e654969a66bf24ed
AlvaroGomezNerpio <alvarogomeznerpio@gmail.com>
2025-01-11 20:12:42 +0100

    1º

---
+++ src/main/java/com/MetaScore/MetaScore/MetaScoreApplication.java
@@ -0,0 +1,13 @@
+package com.MetaScore.MetaScore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MetaScore/MetaScore/MetaScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MetaScore.MetaScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
